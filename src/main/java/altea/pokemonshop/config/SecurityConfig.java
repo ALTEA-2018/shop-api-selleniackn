1fa1233936ae8d0340384c7f0ca3a89cffbca9aa
selleniackn <sellenia.chikhoune.etu@univ-lille.fr>
2019-07-03 16:14:19 +0200

    Add Security config class

---
+++ src/main/java/altea/pokemonshop/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package altea.pokemonshop.config;
+
+import altea.pokemonshop.service.TrainerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private TrainerService trainerService;
+
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.csrf().ignoringAntMatchers("/api/**");
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        List<GrantedAuthority> roles = new ArrayList<>();
+        roles.add(() -> "ROLE_USER");
+        return s ->
+                Optional.ofNullable(this.trainerService.findTrainerByName(s))
+                        .map(trainer -> new User(trainer.getName(), trainer.getPassword(), true, true, true, true, roles))
+                        .orElseThrow(() -> new BadCredentialsException("No such user"));
+    }
+
+    @Autowired
+    public void setTrainerService(TrainerService trainerService) {
+        this.trainerService = trainerService;
+    }
+
+}
