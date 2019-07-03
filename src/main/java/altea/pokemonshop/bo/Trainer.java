8479b7222a41e96c091140571bec280b79701e97
selleniackn <sellenia.chikhoune.etu@univ-lille.fr>
2019-07-03 16:13:06 +0200

    Add class trainer

---
+++ src/main/java/altea/pokemonshop/bo/Trainer.java
@@ -0,0 +1,48 @@
+package altea.pokemonshop.bo;
+
+public class Trainer {
+    private int id;
+    private String name;
+    private String password;
+    private int credits;
+
+    public Trainer() {
+
+    }
+
+    public Trainer(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
+    }
+}
