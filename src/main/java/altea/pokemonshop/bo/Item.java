c31066ffc4aba74017eea7f6d361fabb8e1f792a
selleniackn <sellenia.chikhoune.etu@univ-lille.fr>
2019-07-03 16:36:14 +0200

    Add class Item

---
+++ src/main/java/altea/pokemonshop/bo/Item.java
@@ -0,0 +1,40 @@
+package altea.pokemonshop.bo;
+
+public class Item {
+    private String name;
+    private String description;
+    private int price;
+
+    public Item() {
+
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
+
