aeec0af6dbfba6a32005a037a00d1524d35e350d
selleniackn <sellenia.chikhoune.etu@univ-lille.fr>
2019-07-05 00:56:54 +0200

    add to security config csrf

--- src/main/java/altea/pokemonshop/config/SecurityConfig.java
+++ src/main/java/altea/pokemonshop/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.csrf().ignoringAntMatchers("/api/**");
+        http.csrf().ignoringAntMatchers("/**");
     }
 
     @Bean
