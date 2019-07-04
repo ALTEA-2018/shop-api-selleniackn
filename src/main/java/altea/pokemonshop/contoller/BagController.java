626fc4e95c480f51bf66cca4459984f18a987856
selleniackn <sellenia.chikhoune.etu@univ-lille.fr>
2019-07-04 20:16:10 +0200

    Add annotation Controller

--- src/main/java/altea/pokemonshop/contoller/BagController.java
+++ src/main/java/altea/pokemonshop/contoller/BagController.java
@@ -8,13 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+@Controller
 public class BagController {
     private TrainerService trainerService;
     private ItemService itemService;
@@ -40,3 +41,4 @@ public ModelAndView bag() {
         this.itemService = itemService;
     }
 }
+
